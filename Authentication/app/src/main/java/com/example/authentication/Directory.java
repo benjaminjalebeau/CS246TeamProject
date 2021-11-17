1629eedc5d3fdb5d122174e0b3970d865855b4df
benjaminjalebeau <72671113+benjaminjalebeau@users.noreply.github.com>
2021-11-17 10:43:13 -0700

    Add files via upload

--- Authentication/app/src/main/java/com/example/authentication/Directory.java
+++ Authentication/app/src/main/java/com/example/authentication/Directory.java
@@ -6,6 +6,7 @@ public class Directory {
 
     public ArrayList projectList;
     public ArrayList archiveList;
+    public ArrayList employeeList;
 
     public PianoProject createProject(String serial, String model, String startedDate, Boolean priority){
         PianoProject newProject = new PianoProject();
@@ -18,6 +19,22 @@ public class Directory {
 
     public void addProject(PianoProject project){
         projectList.add(project);
+    }
+
+    public void archiveProject(PianoProject project){
+        projectList.remove(project);
+        archiveList.add(project);
+    }
+
+    public Employee createEmployee(String employeeName, String password){
+        Employee newEmployee = new Employee();
+        newEmployee.setEmployeeName(employeeName);
+        newEmployee.setPassword(password);
+        return newEmployee;
+    }
+
+    public void addEmployee(Employee newEmployee){
+        employeeList.add(newEmployee);
     }
 
 }
---
+++ Authentication/app/src/main/java/com/example/authentication/Employee.java
@@ -0,0 +1,22 @@
+package com.example.authentication;
+
+public class Employee {
+    private String employeeName;
+    private String password;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
