a38d8c6b3594dc39d0df0102ebbdd0591333050d
kjolley4 <91900261+kjolley4@users.noreply.github.com>
2021-11-24 09:53:25 -0700

    Integrating Main Activity into Directory

--- Authentication/app/src/main/java/com/example/authentication/Directory.java
+++ Authentication/app/src/main/java/com/example/authentication/Directory.java
@@ -1,12 +1,35 @@
 package com.example.authentication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 
-public class Directory {
+public class Directory extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
     public ArrayList projectList;
     public ArrayList archiveList;
     public ArrayList employeeList;
+
+/*
+Give the user ability to logout of app and be redirected to the Login Activity.
+ */
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
+    }
+}
 
     public PianoProject createProject(String serial, String model, String startedDate, Boolean priority){
         PianoProject newProject = new PianoProject();
